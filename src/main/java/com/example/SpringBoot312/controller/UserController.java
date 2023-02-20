94effc4806eb647417665367e49da88774a974a9
Roflex24 <am-2000@mail.ru>
2023-02-20 14:26:39 +0300

    update

--- src/main/java/com/example/SpringBoot312/controller/UserController.java
+++ src/main/java/com/example/SpringBoot312/controller/UserController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
-
 @Controller
 public class UserController {
 
@@ -23,8 +21,7 @@ public class UserController {
 
     @GetMapping("/users")
     public String findAll(Model model) {
-        List<User> users = userService.findAll();
-        model.addAttribute("users", users);
+        model.addAttribute("users", userService.findAll());
         return "user-list";
     }
 
@@ -47,8 +44,7 @@ public class UserController {
 
     @GetMapping("/user-update/{id}")
     public String updateUserForm(@PathVariable("id") Long id, Model model) {
-        User user = userService.findById(id);
-        model.addAttribute("user", user);
+        model.addAttribute("user", userService.findById(id));
         return "/user-update";
     }
 
